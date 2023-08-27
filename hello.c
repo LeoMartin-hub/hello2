a8a3ee86d4d05e46804256f53d745869a5eafa69
user158756 <lmartin32@husker.unl.edu>
2023-08-27 13:27:34 -0500

    redo commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World Program
+ */
+
+ int main(int agrc, char **argv) {
+
+    printf("Hello, I'm Leo Martin!\n");
+     printf("My Major is Computer Science!\n");
+
+     return 0;
+ }
